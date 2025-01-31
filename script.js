da2ab7ea5443e93148675102dff212f2d41ea25c
Zahkrae <xyzbullet001@gmail.com>
2025-02-01 03:18:13 +0800

    fix2

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function playAlert(option) {
     let messages = {
-        'option1_1': { text: "Great choice!", sound: "sounds/correct.mp3", next: "question2.html" },
-        'option1_2': { text: "Nice pick!", sound: "sounds/correct.mp3", next: "question2.html" },
-        'option1_3': { text: "Interesting!", sound: "sounds/correct.mp3", next: "question2.html" },
-        'option1_4': { text: "Oops! Try again.", sound: "sounds/wrong.mp3", next: "gameover.html" }
-		'option2_1': { text: "Great choice!", sound: "sounds/correct.mp3", next: "question3.html" },
-        'option2_2': { text: "Nice pick!", sound: "sounds/wrong.mp3", next: "gameover.html" },
-        'option2_3': { text: "Interesting!", sound: "sounds/correct.mp3", next: "question3.html" },
-        'option2_4': { text: "Oops! Try again.", sound: "sounds/wrong.mp3", next: "gameover.html" }
-		'option3_1': { text: "Great choice!", sound: "sounds/wrong.mp3", next: "gameover.html" },
-        'option3_2': { text: "Nice pick!", sound: "sounds/wrong.mp3", next: "gameover.html" },
-        'option3_3': { text: "Interesting!", sound: "sounds/wrong.mp3", next: "gameover.html" },
-        'option3_4': { text: "Oops! Try again.", sound: "sounds/correct.mp3", next: "question4.html" }
-		'option4_1': { text: "Great choice!", sound: "sounds/correct.mp3", next: "success.html" },
-        'option4_2': { text: "Nice pick!", sound: "sounds/wrong.mp3", next: "gameover.html" },
+        option1_1: { text: "Great choice!", sound: "sounds/correct.mp3", next: "question2.html" },
+        option1_2: { text: "Nice pick!", sound: "sounds/correct.mp3", next: "question2.html" },
+        option1_3: { text: "Interesting!", sound: "sounds/correct.mp3", next: "question2.html" },
+        option1_4: { text: "Oops! Try again.", sound: "sounds/wrong.mp3", next: "gameover.html" }
+		option2_1: { text: "Great choice!", sound: "sounds/correct.mp3", next: "question3.html" },
+        option2_2: { text: "Nice pick!", sound: "sounds/wrong.mp3", next: "gameover.html" },
+        option2_3: { text: "Interesting!", sound: "sounds/correct.mp3", next: "question3.html" },
+        option2_4: { text: "Oops! Try again.", sound: "sounds/wrong.mp3", next: "gameover.html" }
+		option3_1: { text: "Great choice!", sound: "sounds/wrong.mp3", next: "gameover.html" },
+        option3_2: { text: "Nice pick!", sound: "sounds/wrong.mp3", next: "gameover.html" },
+        option3_3: { text: "Interesting!", sound: "sounds/wrong.mp3", next: "gameover.html" },
+        option3_4: { text: "Oops! Try again.", sound: "sounds/correct.mp3", next: "question4.html" }
+		option4_1: { text: "Great choice!", sound: "sounds/correct.mp3", next: "success.html" },
+        option4_2: { text: "Nice pick!", sound: "sounds/wrong.mp3", next: "gameover.html" },
 
     };
 
@@ -22,7 +22,8 @@ function playAlert(option) {
         audio.play();
         setTimeout(() => {
             alert(messages[option].text);
+            setTimeout(() => {
             window.location.href = messages[option].next;
-        }, 100); // Delay to allow sound to start playing
+        }, 3000);
     }
 }
